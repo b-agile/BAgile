56de2e39bcc3eb339c8988b08a6152fd873a2816
bagile <contact@bagile.net>
2014-02-27 23:37:47 +0100

    Allow to commit secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BAgile::Application.config.secret_key_base = '45111372ca7a7886c524f95245a58c82be00edc5bd01082bc2e870f4c3d0b612f3e94049e52af517529932eb9f8083ee795b2931c83f8e5d80d988c167d7af75'
