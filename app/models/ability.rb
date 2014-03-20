d6d685812a30b5e4626d5b863e6ba2b07fee0e47
bagile <contact@bagile.net>
2014-03-20 22:33:22 +0100

    Allow closed web site

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,6 +13,11 @@ class Ability
       can :manage, Project
       can :manage, Sprint
     end
+
+
+    if ENV['CAN_REGISTER'] != 'admin-only'
+      can :create, User
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
